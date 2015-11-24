3781c26a5db480f86614a5dbf6ef3478bdc41164
Artur Meletyan <artm95@gmail.com>
2015-11-24 14:02:19 -0700

    database.getUser() now adds names to list

--- src/database.py
+++ src/database.py
@@ -1,14 +1,17 @@
 import mysql.connector
 
-
 conn = mysql.connector.connect(user='tempuser', password='password', database='project_brian_test')
 cursor = conn.cursor()
 
 def getUsers(board_name):
     query = "SELECT name FROM users JOIN boards ON boards.user_id = users.user_id WHERE board_name = '%s'" % board_name
     cursor.execute(query)
+    nameList = []
     for name in cursor:
-        print name
+        nameList.append(name)
+
+    print(nameList)
+    #return nameList
 
 def main():
     board_name = 'temp board'
