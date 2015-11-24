460d2d57cf0201be8dd6891231da6c16d250444c
Artur Meletyan <artm95@gmail.com>
2015-11-24 16:03:41 -0700

    implemented some more methods on database.py

--- src/database.py
+++ src/database.py
@@ -4,50 +4,75 @@ conn = mysql.connector.connect(user='tempuser', password='password', database='p
 cursor = conn.cursor()
 
 # user: sign up
+#	NEED TO FIGURE HOW LOGIN SYSTEM IS GONNA WORK
 def signUp(user_name):
 	return 0
 
 # user: login
+#	NEED TO FIGURE HOW LOGIN SYSTEM IS GONNA WORK
 def login(user_name):
 	return 0
 
 # leader: make board
+#	NEED TO FIGURE OUT QUERY TO ADD TO SQL TABLE
 def makeBoard(leader_name):
 	return 0
 
 # leader: add members to board (?)
 
 # user: see all tasks
-def seeAllTasks(user_name):
-	return 0
+#	this method will probably be called initially by default as opposed to seeOwnTasks()
+def seeAllTasks():
+	query = "SELECT * FROM tasks" % user_name
+	cursor.execute(query)
+	taskList = []
+
+	for task in cursor:
+		taskList.append(task)
+
+	print(taskList)
+	#return taskList
 
 # leader: make tasks/assign members
 # 	"members" is a list of members to assign
 #	"task" is the description of the task
+#	NEED TO FIGURE OUT QUERY TO ADD TO SQL TABLE
 def makeTask(task, members):
 	return 0
 
 # task: move to Backlog
+#	part of the State DP, check slides/notes to implement
 def toBacklog(task):
 	return 0
 
 # task: move to In Progress
+#	part of the State DP, check slides/notes to implement
 def toInProgress(task):
 	return 0
 
 # task: move to Done
+#	part of the State DP, check slides/notes to implement
 def toDone(task):
 	return 0
 
 # user: see user's tasks only
 def seeOwnTasks(user_name):
-	return 0
+	query = "SELECT * FROM tasks WHERE user_name = '%s'" % user_name
+	cursor.execute(query)
+	taskList = []
+
+	for task in cursor:
+		taskList.append(task)
+
+	print(taskList)
+	#return taskList
 
 # board: get users
 def getUsers(board_name):
     query = "SELECT name FROM users JOIN boards ON boards.user_id = users.user_id WHERE board_name = '%s'" % board_name
     cursor.execute(query)
     nameList = []
+
     for name in cursor:
         nameList.append(name)
 
@@ -59,6 +84,7 @@ def getTasks(board_name):
 	query = "SELECT tasks FROM boards WHERE board_name = '%s'" % board_name
 	cursor.execute(query)
 	taskList = []
+	
 	for task in cursor:
 		taskList.append(task)
 
